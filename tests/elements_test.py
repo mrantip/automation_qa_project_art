c9c2a60f3c1a7dae3b0eff83181d5fb19b15d755
Sergei Antipov <mrantip90@gmail.com>
2023-11-30 20:38:54 +0300

    v3 addeed locators, element_page.py and tests

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,9 +1,19 @@
 import time
 
 from pages.base_page import BasePage
+from pages.element_page import TextBoxPage
 
 
-def test_1(driver):
-    page = BasePage(driver, 'https://www.google.com/')
-    page.open()
-    time.sleep(3)
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+            print(output_name)
+            print(output_email)
+            print(output_cur_addr)
+            print(output_per_addr)
+
