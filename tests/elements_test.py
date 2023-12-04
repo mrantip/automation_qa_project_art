32f82e2b5a67c63ad4418d23c83e2da4f3949cab
Sergei Antipov <mrantip90@gmail.com>
2023-12-04 20:01:16 +0300

    v3 addeed generatirs, elements_test.py is finished

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -10,10 +10,13 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            # input_data = text_box_page.fill_all_fields()
+            # output_data = text_box_page.check_filled_form()
+            # assert input_data == output_data
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_cur_addr)
-            print(output_per_addr)
+            assert full_name == output_name, 'full name does not match'
+            assert email == output_email, 'email does not match'
+            assert current_address == output_cur_addr, 'fcurrent_address does not match'
+            assert permanent_address == output_per_addr, 'permanent_address does not match'
 
