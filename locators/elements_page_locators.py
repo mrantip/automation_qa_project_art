86d97c41f589ee352ef894a4547885e975c2f6cf
Sergei Antipov <mrantip90@gmail.com>
2023-12-11 21:00:46 +0300

    v6 webtable, check_new_added_person added

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -33,6 +33,7 @@ class RadioButtonPageLocators:
 
 
 class WebTablePageLocators:
+    # add person form
     ADD_BUTTON = (By.CSS_SELECTOR, 'button[id="addNewRecordButton"]')
     FIRSTNAME_INPUT = (By.CSS_SELECTOR, 'input[id="firstName"]')
     LASTNAME_INPUT = (By.CSS_SELECTOR, 'input[id="lastName"]')
@@ -41,3 +42,6 @@ class WebTablePageLocators:
     SALARY_INPUT = (By.CSS_SELECTOR, 'input[id="salary"]')
     DEPARTMENT_INPUT = (By.CSS_SELECTOR, 'input[id="department"]')
     SUBMIT = (By.CSS_SELECTOR, 'button[id="submit"]')
+
+    # table
+    FULL_PEOPLE_LIST = (By.CSS_SELECTOR, 'div[class="rt-tr-group"]')
