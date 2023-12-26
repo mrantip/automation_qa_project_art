b66d8062b4551d4cf9f6bb049ca8f0f5e4236973
Sergei Antipov <mrantip90@gmail.com>
2023-12-26 21:06:44 +0300

    v14  added
    class:
    TestFormPage
    
    tests:
    test_form

--- tests/form_test.py
+++ tests/form_test.py
@@ -1,8 +1,14 @@
+import time
+
 from pages.form_page import FormPage
 
 
-class TestForm:
+class TestFormPage:
     class TestFormPage:
         def test_form(self, driver):
             form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
             form_page.open()
+            person_info = form_page.fill_form_fields()
+            result = form_page.form_result()
+            assert [person_info.firstname + ' ' + person_info.lastname, person_info.email] == [result[0], result[
+                1]], 'The form has not been filled'
