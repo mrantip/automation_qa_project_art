b8c7dc2cb5e9bf416a0d04e7c37dfedc96717ac8
Sergei Antipov <mrantip90@gmail.com>
2024-01-11 19:00:19 +0300

    v35  added tests: test_axis_restricted_draggable

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -55,3 +55,14 @@ class DroppablePageLocators:
     WILL_REVERT = (By.CSS_SELECTOR, 'div[id="revertable"]')
     NOT_REVERT = (By.CSS_SELECTOR, 'div[id="notRevertable"]')
     DROP_HERE_REVERT = (By.CSS_SELECTOR, '#revertableDropContainer #droppable')
+
+
+class DragabblePageLocators:
+    # Simple
+    SIMPLE_TAB = (By.CSS_SELECTOR, 'a[id="draggableExample-tab-simple"]')
+    DRAG_ME = (By.CSS_SELECTOR, '#draggableExample-tabpane-simple #dragBox')
+
+    # Axis Restricted
+    AXIS_TAB = (By.CSS_SELECTOR, 'a[id="draggableExample-tab-axisRestriction"]')
+    ONLY_X = (By.CSS_SELECTOR, 'div[id="restrictedX"]')
+    ONLY_Y = (By.CSS_SELECTOR, 'div[id="restrictedY"]')
